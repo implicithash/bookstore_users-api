87a4e92359d70b58847787f04e8f0414525031dc
Vilena Aleksandrova <alviro12@gmail.com>
2020-11-02 09:57:52 +0500

    dao structure

---
+++ app/url_mappings.go
@@ -0,0 +1,13 @@
+package app
+
+import (
+	"github.com/implicithash/bookstore_users-api/controllers/ping"
+	"github.com/implicithash/bookstore_users-api/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	//router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", users.CreateUser)
+}
---
+++ controllers/users/users_controller.go
@@ -0,0 +1,44 @@
+package users
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/implicithash/bookstore_users-api/domain/users"
+	"github.com/implicithash/bookstore_users-api/services"
+	"github.com/implicithash/bookstore_users-api/utils/errors"
+	"net/http"
+	"strconv"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.BadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.BadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+
+/*func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}*/
